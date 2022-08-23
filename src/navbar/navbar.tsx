d4a46057b49da3d9a83cf94550c180f0a85b32e9
JoaoTeixeira13 <j.teixeira.dev@gmail.com>
2022-08-23 16:43:14 +0200

    Navbar component, media queries for mobile (hamburger menu)

--- src/navbar/navbar.tsx
+++ src/navbar/navbar.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./navbar.css";
 import * as Scroll from "react-scroll";
 import {
@@ -9,11 +10,15 @@ import {
     scrollSpy,
     scroller,
 } from "react-scroll";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 export default function Navbar() {
+    const [toggle, setToggle] = useState(false);
+
     return (
-        <div id="navbar">
-            <div>
+        <nav id="navbar">
+            <div className="appNavbar">
                 <Link
                     activeClass="active"
                     to="about"
@@ -55,6 +60,65 @@ export default function Navbar() {
                     <h4>Contact</h4>
                 </Link>
             </div>
-        </div>
+            <div className="appNavbarMobile">
+                <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+                {toggle && (
+                    <motion.div
+                        whileInView={{ x: [300, 30] }}
+                        transition={{ duration: 0.85, ease: "easeOut" }}
+                    >
+                        <div className="mobileSlide">
+                            <HiX
+                                className="closingMobile"
+                                onClick={() => setToggle(false)}
+                            />
+                            <Link
+                                activeClass="active"
+                                to="about"
+                                spy={true}
+                                smooth={true}
+                                // offset={50}
+                                duration={500}
+                            >
+                                <h4 onClick={() => setToggle(false)}>About</h4>
+                            </Link>
+                            <Link
+                                activeClass="active"
+                                to="work"
+                                spy={true}
+                                smooth={true}
+                                // offset={50}
+                                duration={500}
+                            >
+                                <h4 onClick={() => setToggle(false)}>Work</h4>
+                            </Link>
+                            <Link
+                                activeClass="active"
+                                to="skills"
+                                spy={true}
+                                smooth={true}
+                                // offset={50}
+                                duration={500}
+                            >
+                                <h4 onClick={() => setToggle(false)}>Skills</h4>
+                            </Link>
+                            <Link
+                                activeClass="active"
+                                to="contact"
+                                spy={true}
+                                smooth={true}
+                                // offset={50}
+                                duration={500}
+                            >
+                                <h4 onClick={() => setToggle(false)}>
+                                    Contact
+                                </h4>
+                            </Link>
+                        </div>
+                    </motion.div>
+                )}
+            </div>
+        </nav>
     );
 }
